38db975a70a4b22678217455a5c896d5cd3096ac
JakobSpiss <jakobspiss@tsn.at>
2022-11-16 08:41:22 +0100

    Decorator- Klasse Hashdecorator erstellt.

---
+++ src/main/java/at/itkolleg/credentials/Hashdecorator.java
@@ -0,0 +1,16 @@
+package at.itkolleg.credentials;
+
+import java.util.List;
+
+public abstract class Hashdecorator implements ExportCredentials{
+
+    ExportCredentials credentials;
+
+    public Hashdecorator (ExportCredentials credencials){
+        this.credentials = credencials;
+    }
+    @Override
+    public void export(List<Credentials> credentialsList) {
+
+    }
+}
